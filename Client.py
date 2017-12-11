86f83b1c5f92d572776501f4b661d65ad1098db5
Alexx Bull <alexxbull@users.noreply.github.com>
2017-12-10 22:39:22 -0800

    1. Removed threads implementation as classes and instead use a single line declaration 2. Removed unnecessary threads 3. GUI: moved Client's send button to the button of the window 4. GUI: changed Client's send button color to match the MS Window's blue title bar color 5. Fixed bug in Client.py that would cause the script to continue running after being (force) disconnected from server.

--- Client.py
+++ Client.py
@@ -1,87 +1,88 @@
+import os
 import socket
 import threading
+import time
 
-client = socket.socket()
-
-class ReceivingThread(threading.Thread):
-    """A thread for listening for and receiving new messages from the server"""
-
-    def run(self):
-        """Listen for messages from the server and display them."""
-        try:
-            receive_message()
-        except socket.error as err:
-            print("Lost connection with server.")
-
-class SendingThread(threading.Thread):
-    """A thread for sending messages from to the server"""
-
-    def __init__(self, name):
-        threading.Thread.__init__(self, name = name)
-
-    def run(self):
-        """Listen for messages from the server and display them."""
-        try:
-            send_message()
-        except socket.error as err:
-            print("Lost connection with server.")
+client = socket.socket()                                                            # Client's socket object for, well, being a client
 
 def join_server(h = "localhost", p = 8888):
-    """Connect client to server"""
-    global client
+    "Connect client to server"
     try:
         host = h
         post = p
         client.connect((host, post));
     except socket.error as err:
         print("Failed to connect to server.")
+        print(str(err) + "\nExiting...")
+        time.sleep(2)
+        quit()
     else:
         send_username()
 
 def send_username():
-    global client
+    "Complete username registration and then enter chat room"
     try:
         while True:
-            getName = client.recv(1024)     # read server message for entering username
+            getName = client.recv(1024)                                             # read server message for entering username
             getName = getName.decode()
             name = input(getName)
 
-            client.send(str.encode(name))            # send server the username entered
-            valid = client.recv(1024)       # check if server approves of name
+            client.send(str.encode(name))                                           # send server the username entered
+            valid = client.recv(1024)                                               # check if server approves of name
             valid = valid.decode()
             taken = "Username " + name + " is already taken."
             if valid == taken:
                 print(taken)
                 continue
-            else:
-                rt = ReceivingThread()
-                st = SendingThread(name)
+            elif valid != taken and name != "":
+                rt = threading.Thread(target = receive_message, name = name)        # make a new thread for receiving messages from the sever
                 rt.start()
-                st.start()
+                send_message()                                                      # sending messages to server will be handled in the main thread
                 break
     except socket.error as err:
-        print("Lost connection with server.")
+        print("Lost connection with server.\nExiting...")
+        time.sleep(2)
+        quit()
 
 def receive_message():
-    """Listen for messages from the server and display them."""
-    global client, name
-    while True:
-        text = client.recv(1024)
-        text = text.decode()
-        print(text)
-        if text == "You have left the chat room. Goodbye!":
-            break
+    "Listen for messages from the server and display them."
+    try:
+        while True:
+            text = client.recv(1024)
+            text = text.decode()
+            print(text)
+            if text == "You have left the chat room. Goodbye!":
+                time.sleep(2)
+                break
+    except socket.error as err:
+        print("Lost connection with server.\nExiting...")
+        time.sleep(2)
+        quit()
 
 def send_message():
-    """Send message to server"""
-    global client
-    while True:
-        text = input()
-        text = str.encode(text)
-        client.send(text)
-        if (text.decode()).lower() == "exit":
-            break
-def main():
-    join_server()
+    "Send message to server"
+    try:
+        while True:
+            text = input()
+            text = str.encode(text)
+            if text != "":
+                client.send(text)
+            if (text.decode()).lower() == "exit":
+                break
+    except socket.error as err:
+        print("Lost connection with server.\nExiting...")
+        time.sleep(2)
+        quit()
 
-main()
+def quit():
+    "Close the client's connection and exit the program"
+    client.close()
+    os._exit(0)
+
+join_server()
+
+"""
+Active Threads:
+- Main thread will handle sending messages to server
+- A new thread (rt) is made for receiving message from server
+"""
--- Server.py
+++ Server.py
@@ -1,156 +1,126 @@
 import socket
 import threading
 import sys
+import time
 
-server = socket.socket()
-clients = {}
-lt = None       #variable for listening thread
-rt = None       #variable for response thread
-
-class ListeningThread(threading.Thread):
-    """A thread listening for new connections to the server"""
-
-    def run(self):
-        print("Listening for new clients...")
-        listen_and_connect()
-
-class AddClientThread(threading.Thread):
-    """A thread for handling new clients entering the chat room"""
-
-    def __init__(self, conn, name = "new client"):
-        threading.Thread.__init__(self, name = name)
-        self.client = conn
-
-    def run(self):
-        print("\nAdding new client", self.name)
-        try:
-            add_client(self.client)
-        except ConnectionResetError:
-            client_disconnect(self.name)
-
-class ResponseThread(threading.Thread):
-    """A thread for handling a client's messages"""
-
-    def __init__(self, name):
-        threading.Thread.__init__(self, name = name)
-
-    def run(self):
-        try:
-            print("Listening for responses from", self.name)
-            receive_message(self.name)
-        except ConnectionResetError:
-            client_disconnect(self.name)
-
-    def __str__(self):
-        return self.name
+server = socket.socket()                                                                        # Server's socket object for, well, being a server
+clients = {}                                                                                    # dictionary of all clients connected to server
 
 def start_server(host = "localhost", port = 8888):
-    """Create a server socket and start listening for connections"""
+    "Create a server socket and start listening for connections"
     print("Booting server...")
-    global server, clients, lt
     try:
         server.bind((host, port))
         print("Server now running")
-        lt = ListeningThread()
-        lt.start()
+        listen_and_connect()
     except socket.error as err:
-        print("Failed to start server:\n", str(err))
+        print("Failed to start server:")
+        print(str(err) + "\nExiting...")
+        time.sleep(2)
         sys.exit(1)
 
 def listen_and_connect():
-    """Listen for and accept incoming connections"""
-    global server, clients, lt, rt
+    "Listen for and accept incoming connections"
+    print("Listening for new clients...")
     try:
         while True:
             server.listen(5)
             conn, addr = server.accept()
             tempName = str(addr)
-            act = AddClientThread(conn, tempName)
-            act.start()
+            nct = threading.Thread(target = add_client, args = (conn, tempName), name = tempName)   # make new thread for handling the new client
+            nct.start()
     except socket.error as err:
-        print("Listening and connecting has failed:\n", str(err))
+        print("Listening and connecting has failed:")
+        print(str(err) + "\nExiting...")
+        time.sleep(2)
         sys.exit(1)
 
-def add_client(conn):
-    """Ask the client for their username and, if not taken, add them to the chat room"""
-    global clients
-    while True:
-        conn.send(str.encode("Enter a username: "))
-        print("Acquiring client's username")
-        user = conn.recv(1024)
-        user = user.decode()
+def add_client(conn, tempName):
+    "Ask the client for their username and, if not taken, add them to the chat room"
+    print("\nAdding new client", tempName)
+    try:
+        while True:
+            print("Acquiring username for", tempName)
+            conn.send(str.encode("Enter a username: "))
+            user = conn.recv(1024)
+            user = user.decode()
 
-        if user not in clients:
-            clients[user] = conn
-            rt = ResponseThread(user) # create new thread for the client
-            rt.start()
-            system_message("{0} has entered the chat room.".format(user))
-            count()
-            break
-        else:
-            invalid = "Username " + user + " is already taken."
-            print(invalid)
-            conn.send(str.encode(invalid))
+            if user not in clients and user != "":
+                clients[user] = conn
+                system_message("{0} has entered the chat room.".format(user))
+                count()
+                receive_message(user)
+                break
+            elif user in clients:
+                invalid = "Username " + user + " is already taken."
+                print(invalid)
+                conn.send(str.encode(invalid))
+    except (ConnectionResetError, ConnectionAbortedError):
+        client_disconnect(tempName)
 
 def receive_message(sender):
-    """Receive messages from user"""
-    while True:
-        global clients
-        conn = clients[sender]
-        text = conn.recv(1024)
-        text = text.decode()
+    "Receive messages from user"
+    print("Listening for responses from", sender)
+    try:
+        while True:
+            conn = clients[sender]
+            text = conn.recv(1024)
+            text = text.decode()
 
-        if text.lower() == "exit":
-            goodbye = str.encode("You have left the chat room. Goodbye!")
-            conn.send(goodbye)
-            remove_client(sender)
-            text = "{0} has left the room.".format(sender)
-            system_message(text)
-            count()
-            break
-        else:
-            send_message(sender, text)
+            if text.lower() == "exit":
+                goodbye = str.encode("You have left the chat room. Goodbye!")
+                conn.send(goodbye)
+                remove_client(sender)
+                text = "{0} has left the room.".format(sender)
+                system_message(text)
+                count()
+                break
+            else:
+                send_message(sender, text)
+    except (ConnectionResetError, ConnectionAbortedError):
+        client_disconnect(sender)
 
 def send_message(sender, text):
-    """Forward message from sender to all users"""
+    "Forward message from sender to all users"
     print(sender + " says:", text)
     text = "{0}: {1}".format(sender, text)
     text = str.encode(text)
 
-    global clients
     for user in [user for user in clients if user != sender]:
         clients[user].send(text)
 
 def system_message(text):
-    """Broadcast message to all users"""
+    "Broadcast message to all users"
     print("Brodcast message:", text)
     text = str.encode(text)
 
-    global clients
     for user in clients:
         clients[user].send(text)
 
 def remove_client(user):
-    """Removes the given client from clients dict. and closes their connection"""
-    global clients
+    "Removes the given client from clients dict. and closes their connection"
     clients[user].close()
     del clients[user]
 
 def count():
-    """Broadcast the current number of users in the room"""
+    "Broadcast the current number of users in the room"
     system_message(str(len(clients)) + " user(s) in the room.")
     print()
 
 def client_disconnect(name = ""):
-    if name in clients: # Runs when a ResponseThread exception is caught
+    "Handle (abrupt) client disconnections"
+    if name in clients:                                                                             # Runs when a response thread exception is caught
         remove_client(name)
         print("Connection lost with", name)
         system_message("{0} has left the room.".format(name))
         count()
-    else:   # Runs when a NewClientThread exception is caught
+    else:                                                                                           # Runs when adding new client thread exception is caught
         print("Connection lost while adding", name)
 
-def main():
-    start_server()
+start_server()
 
-main()
+"""
+Active Threads:
+- Main thread will handle listening for new connections/clients
+- A new thread (nct) is made for interacting with clients
+"""
